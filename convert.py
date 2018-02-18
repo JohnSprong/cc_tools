193a6f3a1cfa4cb7a09a6aa237020134a7b4e903
jsprong <jsprong42@gmail.com>
2018-02-18 15:44:21 -0500

    phase 3 additions

--- convert.py
+++ convert.py
@@ -8,7 +8,7 @@ import cc_json_utils
 #Use cc_data_utils.make_cc_data_from_dat() to load pfgd_test.dat
 #print the resulting data
 
-print(cc_dat_utils.make_cc_data_from_dat("data/pfgd_test.dat"))
+# print(cc_dat_utils.make_cc_data_from_dat("data/pfgd_test.dat"))
 
 #Part 2
 input_json_file = "data/test_data.json"
@@ -27,3 +27,5 @@ json_file = open(input_json_levels_file)
 json_file_data = json.load(json_file)
 json_game_data = cc_json_utils.make_cc_data_from_json(json_file_data) #Convert JSON data to cc_data
 cc_dat_utils.write_cc_data_to_dat(json_game_data, "data/jsprong_cc1.dat") #Save converted data to DAT file
+cc_test = cc_dat_utils.make_cc_data_from_dat("data/jsprong_cc1.dat")
+print(cc_test)
