a465e00b7fd3ef27f33a64041bdfcaaf3b0c812e
jsprong <jsprong42@gmail.com>
2018-02-18 17:57:21 -0500

    edits

--- cc_json_utils.py
+++ cc_json_utils.py
@@ -5,7 +5,7 @@ def make_cc_data_from_json(json_file):
     cc_data_file = cc_data.CCDataFile()
 
     for json_level in json_file["levels"]:
-        print(json_level)
+        # print(json_level)
         cc_level = cc_data.CCLevel()
         cc_level.level_number = json_level["level_num"]
         cc_level.time = json_level["time_limit"]
@@ -41,9 +41,8 @@ def make_cc_data_from_json(json_file):
                     monsters.append(cc_data.CCCoordinate(x, y))
                     cc_monster_move_field = cc_data.CCMonsterMovementField(monsters)
                     cc_level.add_field(cc_monster_move_field)
-                    print (monster)
+                    # print (monster)
 
         cc_data_file.levels.append(cc_level)
 
-    # print(cc_data_file)
     return cc_data_file
